278d9c5da78f04f75dda78879d64d938e4c5fafd
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-06-22 19:14:00 +0300

    Add files via upload

---
+++ 02 MCAL/01 GPIO/GPIO_interface.h
@@ -0,0 +1,103 @@
+/************************************************/
+/* Author  : Abdelsattar Muhammad    		   */
+/* Date    : 17 June 2023              	      */
+/* Version : V03                    	   	 */
+/********************************************/
+#ifndef GPIO_INTERFACE_H
+#define GPIO_INTERFACE_H
+
+typedef enum{
+		LOW=0,
+		HIGH,
+		PULL_UP,
+		PULL_DOWN,
+			
+			}Value_t;
+
+/***************   GPIO(PORT) definition  *******************/
+typedef enum {
+		GPIOA=0,
+		GPIOB,
+		GPIOC,
+			 }PORT_t;
+		
+/***************   	PIN definition  	 *******************/
+typedef enum{
+		PIN0=0, 
+		PIN1,
+		PIN2, 
+		PIN3,
+		PIN4, 
+		PIN5, 
+		PIN6, 
+		PIN7, 
+		PIN8, 
+		PIN9, 
+		PIN10,
+		PIN11,
+		PIN12,
+		PIN13,
+		PIN14,
+		PIN15,
+			}PINS_t;
+
+
+
+/***************   	Modes definition  	 *******************/
+typedef enum{
+/*
+	//	PP == Push Pull
+    //	OD == Open Drain
+    //	AF == Alternate Function
+*/
+
+	/***************   INPUT Mode definition   ******************/
+		
+		INPUT_ANALOG=0, 	//0b0000	 	
+	    
+		INPUT_FLOATING=4,	//0b0010
+	    
+		INPUT_PULL_UP=8,	//0b1000 	
+
+        INPUT_PULL_DOWN=8,	//0b1000
+	
+	/***************   OUTPUT Mode definition   ******************/
+		//FOR 2 MHZ
+		OUTPUT_SPEED_2MHZ_PP=2,  		//0b0010 
+    	OUTPUT_SPEED_2MHZ_OD=3, 		//0b0110 
+		OUTPUT_SPEED_2MHZ_AF_PP=10, 	//0b1010 
+		OUTPUT_SPEED_2MHZ_AF_OD=14, 	//0b1110 
+		//FOR 10 MHZ
+		OUTPUT_SPEED_10MHZ_PP=1,	 	//0b0001
+		OUTPUT_SPEED_10MHZ_OD=5, 		//0b0101
+		OUTPUT_SPEED_10MHZ_AF_PP=9, 	//0b1001
+		OUTPUT_SPEED_10MHZ_AF_OD=13, 	//0b1101
+		//FOR 50 MHZ
+		OUTPUT_SPEED_50MHZ_PP=3, 		//0b0011 
+		OUTPUT_SPEED_50MHZ_OD=7, 		//0b0111
+		OUTPUT_SPEED_50MHZ_AF_PP=11,	//0b1011	
+		OUTPUT_SPEED_50MHZ_AF_OD=15, 	//0b1111
+	
+			}PIN_Mode_t;
+		
+		
+		
+
+/***************   Function definition   ******************/
+
+void MGPIO_voidSetPinDirection(PORT_t Copy_u8Port,PINS_t Copy_u8Pin,PIN_Mode_t Copy_u8Mode);
+
+void MGPIO_voidSetPinValue(PORT_t Copy_u8Port,PINS_t Copy_u8Pin,Value_t Copy_u8Value);
+
+void MGPIO_voidSetLowNibbleValue(PORT_t Copy_u8Port,u8 Copy_u8Value);
+
+u8 	 MGPIO_u8GetPinValue(PORT_t Copy_u8Port,PINS_t Copy_u8Pin);
+
+void MGPIO_voidToggleValue(PORT_t Copy_u8Port,PINS_t Copy_u8Pin);
+
+/* This Function Locked Pin Direction Mode */
+
+void MGPIO_voidLockPin(PORT_t Copy_u8Port,PINS_t Copy_u8Pinn);
+
+
+#endif
--- 02 MCAL/01 GPIO/GPIO_program.c
+++ 02 MCAL/01 GPIO/GPIO_program.c
@@ -1,1 +1,257 @@
+/***************************************************/
+/* Author  : Abdelsattar Muhammad    		  */
+/* Date    : 17 June 2023            	     */
+/* Version : V03                        		 */
+/*************************************************/
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
 
+#include "GPIO_interface.h"
+#include "GPIO_private.h"
+#include "GPIO_config.h"
+ 
+void MGPIO_voidSetPinDirection(PORT_t Copy_u8Port,PINS_t Copy_u8Pin,PIN_Mode_t Copy_u8Mode)
+{
+	
+	switch(Copy_u8Port)
+	{
+		
+	case GPIOA :
+			if(Copy_u8Pin <= 7 )	
+			{	
+				GPIO_A->CRL &= ~( (0xF) << ( Copy_u8Pin * 4 ) );
+				GPIO_A->CRL |= ( (Copy_u8Mode) << (Copy_u8Pin * 4) );
+				
+			}
+			else if (Copy_u8Pin <=15 )
+			{
+				
+				Copy_u8Pin -= 8;
+				GPIO_A->CRH &= ~( (0xF) << (Copy_u8Pin * 4 ));
+				GPIO_A->CRH |= ((Copy_u8Mode) << (Copy_u8Pin * 4 ));
+			}
+	break;
+	case GPIOB :
+			if(Copy_u8Pin <= 7)	
+			{	
+				GPIO_B->CRL &= ~((0xF) << ( Copy_u8Pin * 4 ));
+				GPIO_B->CRL |= ((Copy_u8Mode) << (Copy_u8Pin * 4 ));
+			}
+			else if (Copy_u8Pin <=15)
+			{
+				Copy_u8Pin -= 8;
+				GPIO_B->CRH &= ~((0xF) << ( Copy_u8Pin * 4 ));
+				GPIO_B->CRH |= ((Copy_u8Mode) << ( Copy_u8Pin * 4 ));
+			}
+	break;
+
+	case GPIOC :
+
+			if(Copy_u8Pin <= 7)	
+			{	
+				GPIO_C->CRL &= ~((0xF) << (Copy_u8Pin * 4 ));
+				GPIO_C->CRL |= ((Copy_u8Mode) << (Copy_u8Pin * 4 ));	
+			}
+			else if (Copy_u8Pin <=15)
+			{
+				Copy_u8Pin -= 8;
+				GPIO_C->CRH &= ~((0xF) << (Copy_u8Pin * 4 ) );
+				GPIO_C->CRH |= (( Copy_u8Mode )<< (Copy_u8Pin * 4 ));
+			}
+	break;
+
+	
+	default : 
+	/* #error */
+	break;
+	}
+
+
+
+}
+
+void MGPIO_voidSetPinValue(PORT_t Copy_u8Port,PINS_t Copy_u8Pin,Value_t Copy_u8Value )
+{
+	switch(Copy_u8Port)
+	{
+	
+		case GPIOA :
+			if(Copy_u8Value == HIGH )
+			{
+			GPIO_A->BSRR=(1 << Copy_u8Pin);
+			}
+			else if(Copy_u8Value == LOW )
+			{
+			GPIO_A->BRR=(1 << Copy_u8Pin);
+			}
+			/************   if Mode Is Input PULL Down or Up  *********/
+			else if(Copy_u8Value == PULL_UP)
+			{ 
+			SET_BIT(GPIO_A->ODR,Copy_u8Pin);	
+			}
+			
+			else if(Copy_u8Value == PULL_DOWN)
+			{
+			CLR_BIT(GPIO_A->ODR,Copy_u8Pin);
+			}
+			
+		break;
+		case GPIOB :
+			if(Copy_u8Value == HIGH )
+			{
+			GPIO_B->BSRR=(1 << Copy_u8Pin);
+			}
+			else if(Copy_u8Value == LOW )
+			{
+			GPIO_B->BRR=(1 << Copy_u8Pin);
+			}
+			/************   if Mode Is Input PULL Down or Up  *********/
+			else if(Copy_u8Value == PULL_UP)
+			{ 
+			SET_BIT(GPIO_B->ODR,Copy_u8Pin);	
+			}
+			
+			else if(Copy_u8Value == PULL_DOWN)
+			{
+			CLR_BIT(GPIO_B->ODR,Copy_u8Pin);
+			}
+		break;
+		case GPIOC :
+			if(Copy_u8Value == HIGH )
+			{
+			GPIO_C->BSRR=(1 << Copy_u8Pin);
+			}
+			else if(Copy_u8Value == LOW )
+			{
+			GPIO_C->BRR=(1 << Copy_u8Pin);
+			}
+			/************   if Mode Is Input PULL Down or Up  *********/
+			else if(Copy_u8Value == PULL_UP)
+			{ 
+			SET_BIT(GPIO_C->ODR,Copy_u8Pin);	
+			}
+			
+			else if(Copy_u8Value == PULL_DOWN)
+			{
+			CLR_BIT(GPIO_C->ODR,Copy_u8Pin);
+			}
+		break;	
+			 
+		default :
+		break ;
+		
+	}
+
+}
+
+void MGPIO_voidToggleValue(u8 Copy_u8Port,u8 Copy_u8Pin)
+{
+	switch(Copy_u8Port)
+	{
+		case GPIOA :
+			if(GET_BIT(GPIO_A->ODR,Copy_u8Pin) == LOW )
+			{
+			GPIO_A->BSRR=(1 << Copy_u8Pin);
+			}
+			else if(GET_BIT(GPIO_A->ODR,Copy_u8Pin) == HIGH )
+			{
+			GPIO_A->BRR=(1 << Copy_u8Pin);
+			}
+		break;
+		case GPIOB :
+			if(GET_BIT(GPIO_B->ODR,Copy_u8Pin) == LOW )
+			{
+			GPIO_B->BSRR=(1 << Copy_u8Pin);
+			}
+			else if(GET_BIT(GPIO_B->ODR,Copy_u8Pin) == HIGH)
+			{
+			GPIO_B->BRR=(1 << Copy_u8Pin);
+			}
+		break;
+		case GPIOC :
+			if(GET_BIT(GPIO_C->ODR,Copy_u8Pin) == LOW )
+			{
+			GPIO_C->BSRR=(1 << Copy_u8Pin);
+			}
+			else if(GET_BIT(GPIO_C->ODR,Copy_u8Pin) == HIGH )
+			{
+			GPIO_C->BRR=(1 << Copy_u8Pin);
+			}
+		break;	
+			 
+		default :
+		break ;
+		
+	}
+
+}
+
+void MGPIO_voidSetLowNibbleValue(PORT_t Copy_u8Port,u8 Copy_u8Value)
+{
+	
+	switch(Copy_u8Port)
+	{
+		case GPIOA :
+		GPIO_A->ODR = (GPIO_A->ODR & 0xFF00) | ( 0x00FF & Copy_u8Value);
+		break;
+		case GPIOB :
+		GPIO_B->ODR = (GPIO_B->ODR & 0xFF00) | ( 0x00FF & Copy_u8Value);
+		break;
+		case GPIOC :
+		GPIO_C->ODR = (GPIO_C->ODR & 0xFF00) | ( 0x00FF & Copy_u8Value);
+		break;
+		
+		default:
+		/*Error*/
+		break;
+	}
+	
+	
+}
+
+u8 MGPIO_u8GetPinValue(PORT_t Copy_u8Port,PINS_t Copy_u8Pin )
+{
+	u8 return_Val=0; 
+	
+	switch(Copy_u8Port)
+	{
+		case GPIOA :
+		return_Val=GET_BIT(GPIO_A->IDR,Copy_u8Pin);
+		break;
+		case GPIOB :
+		return_Val=GET_BIT(GPIO_B->IDR,Copy_u8Pin);
+		break;
+		case GPIOC :
+		return_Val=GET_BIT(GPIO_C->IDR,Copy_u8Pin);
+		break;
+		
+		default:
+		/*Error*/
+		break;
+	}
+	return return_Val;
+
+
+}
+
+
+void MGPIO_voidLockPin(PORT_t Copy_u8Port,PINS_t Copy_u8Pin)
+{
+	switch(Copy_u8Port)
+	{
+		case GPIOA :
+		GPIO_A->LCKR = (1 << Copy_u8Pin);
+		break;
+		case GPIOB :
+		GPIO_B->LCKR = (1 << Copy_u8Pin);
+		break;
+		case GPIOC :
+		GPIO_C->LCKR = (1 << Copy_u8Pin);
+		break;
+		
+		default:
+		/*Error*/
+		break;
+	}
+	
+}
